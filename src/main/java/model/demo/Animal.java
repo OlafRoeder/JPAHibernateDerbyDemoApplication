9ab1c114789ecd1426d8fd078018d2495c11950f
Olaf Roeder <github@olaf-roeder.de>
2020-11-19 20:02:35 +0100

    annotated with @Data;

--- src/main/java/model/demo/Animal.java
+++ src/main/java/model/demo/Animal.java
@@ -1,11 +1,14 @@
 package model.demo;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Animal {
 
     @Id
