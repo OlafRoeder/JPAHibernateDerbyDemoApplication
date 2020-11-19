e9e18f85783a03296c27c40ff17b385d27edb8aa
Olaf Roeder <github@olaf-roeder.de>
2020-11-19 20:01:57 +0100

    annotated with @Entity and @ID;

--- src/main/java/model/demo/Animal.java
+++ src/main/java/model/demo/Animal.java
@@ -1,10 +1,19 @@
 package model.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
     private Type type;
     private String subtype;
     private int weight;
-
 
 }
