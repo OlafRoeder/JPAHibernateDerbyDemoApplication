cc8fa12630e5d359bc67d5bd4fa9c4dd61aa1d53
Olaf Roeder <github@olaf-roeder.de>
2020-11-19 19:35:00 +0100

    added model example with animals;

---
+++ src/main/java/model/demo/Animal.java
@@ -0,0 +1,10 @@
+package model.demo;
+
+public class Animal {
+
+    private Type type;
+    private String subtype;
+    private int weight;
+
+
+}
---
+++ src/main/java/model/demo/Type.java
@@ -0,0 +1,10 @@
+package model.demo;
+
+public enum Type {
+
+    BIRD,
+    FISH,
+    MAMMAL,
+    REPTILE,
+    AMPHIBIAN
+}
