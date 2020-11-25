a38ddd5922f748cbbc211471d02e60479a56aa9a
Olaf Roeder <github@olaf-roeder.de>
2020-11-25 19:57:50 +0100

    created minimalistic view;

--- src/main/java/controller/MainWindowController.java
+++ src/main/java/controller/MainWindowController.java
@@ -2,15 +2,33 @@ package controller;
 
 import application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import lombok.NonNull;
+import model.Type;
 
 public class MainWindowController extends VBox {
 
     private final Application application;
 
+    @FXML
+    private TextField name;
+
+    @FXML
+    private Spinner<Integer> age;
+
+    @FXML
+    private ComboBox<Type> type;
+
     public MainWindowController(@NonNull Application application) {
         this.application = application;
+    }
+
+    @FXML
+    private void initialize() {
+        type.getItems().setAll(Type.values());
     }
 
     @FXML
